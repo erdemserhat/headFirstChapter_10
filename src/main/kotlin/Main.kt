896bb21b90bf5d74f87421eb0d0706648f5219e8
Serhat <me.serhaterdem@gmail.com>
2023-10-31 20:08:56 +0300

    -GENERICS -INTRODUCTION TO COVARIANCE -out keyword

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -45,6 +45,51 @@ fun main(args:Array<String>){
 
 
        val petRetailer:Retailer<Pet> = CatRetailer()
+       petRetailer.sell()
+
+       //Okey but what is the goal of covariance??
+
+       /**
+        * Covariance is a concept that allows more specific subtypes
+        * to be used in place of more general supertypes.
+        * This can be especially useful when working with generic types,
+        * making your code more flexible and user-friendly.
+        * Here are some comments about the purpose and
+        * benefits of covariance:
+        */
+
+       /**
+        * 1. Greater General Use:
+        * Covariance allows you to place a more specific type on top of
+        * a more general type. This makes your code more flexible
+        * and supports a wider range of types. This is particularly
+        * important for data structures like collections.
+        * For example, you can assign a 'List<Cat>' type to a
+        * 'List<Pet>' type.
+        */
+
+       /**
+        * 2. User-Friendly:
+        * Covariance makes APIs easier to use, reducing the need
+        * for type conversions and improving usability.
+        * It helps in writing more concise and user-friendly code.
+        */
+
+       /**
+        * 3. Reducing Pointer Problems:
+        * Covariance reduces pointer problems, such as type
+        * incompatibility. It leads to safer and error-free code.
+        * It ensures that you can work with different subtypes of a
+        * common supertype more seamlessly.
+        */
+
+       /**
+        * While covariance is a powerful feature, it should be
+        * used carefully, as misuse can lead to runtime errors.
+        * Understanding type parameters and variance rules is
+        * essential when working with covariance.
+        */
+
 
 
 
